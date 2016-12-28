4dcacc950725adfd0a18b88dd3e0ef7432ed260e
Hercules Merscher <hlmerscher@gmail.com>
2016-12-27 22:02:51 -0200

    random numbers ordered function extracted

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 extern crate rand;
 
-fn main() {
+fn random_numbers_ordered() -> Vec<i32> {
     let mut rng = rand::thread_rng();
     let range = 1..61;
     let mut sample = rand::sample(&mut rng, range, 6);
     sample.sort();
-    for n in sample {
+    return sample;
+}
+
+fn main() {
+    let numbers = random_numbers_ordered();
+    for n in numbers {
         print!("{} ", n);
     }
     println!();
